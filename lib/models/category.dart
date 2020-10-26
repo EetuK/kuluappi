39e89e2dc607bf5ec2f7fb0fd81e921193f45072
EetuK <eetukoskelainen@hotmail.com>
2020-10-26 19:07:26 +0200

    Initial app

---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+class Category {
+  num id;
+  String name;
+
+  Category(num id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+// Look e.g. expense.dart file
---
+++ lib/models/expense.dart
@@ -0,0 +1,41 @@
+class Expense {
+  num id;
+  num amount;
+  String description;
+  String timestamp;
+  num categoryId;
+
+  Expense(num id, num amount, String description, String timestamp,
+      num categoryId) {
+    this.id = id;
+    this.amount = amount;
+    this.description = description;
+    this.timestamp = timestamp;
+    this.categoryId = categoryId;
+  }
+}
+
+enum Month {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+// Here all the functions for Expense db operations e.g.
+
+Future<Expense> getExpenseById(num id) {
+  // ...
+}
+
+Future<List<Expense>> getExpensesByMonth(Month month) {
+  // ...
+}
