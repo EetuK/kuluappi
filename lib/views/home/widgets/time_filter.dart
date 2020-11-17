677f30ff0204bbb633e5412c16b30d53f17d729b
Eetu <35661367+EetuK@users.noreply.github.com>
2020-11-17 08:57:56 +0200

    Adding dropdown filters (#8)
    Co-authored-by: EetuK <eetukoskelainen@hotmail.com>

--- lib/views/home/widgets/time_filter.dart
+++ lib/views/home/widgets/time_filter.dart
@@ -5,29 +5,59 @@ class TimeFilter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const bool noExpenses = true;
-
     return Container(
       height: 70,
       color: Colors.orange[50],
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        // Container(
-        //   child: TextField(
-        //     obscureText: true,
-        //     decoration: InputDecoration(
-        //       border: OutlineInputBorder(),
-        //       labelText: 'Password',
-        //     ),
-        //   ),
-        // ),
-        // Container(
-        //     child: TextField(
-        //   obscureText: true,
-        //   decoration: InputDecoration(
-        //     border: OutlineInputBorder(),
-        //     labelText: 'Password',
-        //   ),
-        // ))
+        DropdownButton<String>(
+            value: "Tammikuu",
+            icon: Icon(Icons.arrow_drop_down),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.black, fontSize: 18),
+            underline: Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            onChanged: (String newValue) {},
+            items: <String>[
+              'Tammikuu',
+              'Helmikuu',
+              'Maaliskuu',
+              'Huhtikuu',
+              'Toukokuu',
+              'Kesäkuu',
+              'Heinäkuu',
+              'Elokuu',
+              'Syyskuu',
+              'Lokakuu',
+              'Marraskuu',
+              'Joulukuu'
+            ].map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList()),
+        DropdownButton<String>(
+          value: "2020",
+          icon: Icon(Icons.arrow_drop_down),
+          iconSize: 24,
+          elevation: 16,
+          style: TextStyle(color: Colors.black, fontSize: 18),
+          underline: Container(
+            height: 2,
+            color: Colors.black,
+          ),
+          onChanged: (String newValue) {},
+          items: <String>['2019', '2020']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        )
       ]),
     );
   }
