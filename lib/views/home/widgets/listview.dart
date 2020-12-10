7cc24340c5284a760cbd99872a1e595a4ecab317
Ville Paakkunainen <ville.paakkunainen@tuni.fi>
2020-12-10 14:06:15 +0200

    list_vieww

--- lib/views/home/widgets/listview.dart
+++ lib/views/home/widgets/listview.dart
@@ -3,22 +3,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:kuluappi/stores/expense_store.dart';
 import 'package:provider/provider.dart';
+import 'package:kuluappi/views/modify_expense/modify_expense_view.dart';
 
 class Lists extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Listview(title: "Beer"),
-      Listview(title: "Food"),
-      Listview(title: "Other"),
+      Listview(title: "Beer", expenses: [
+        "Keisari Micro IPA",
+        "Keisari APA",
+        "Karhu 0.33L"
+    ], prices: [
+      1.63,
+      3.06,
+      1.21
+    ], days: [
+      21,
+      10,
+      3
+    ]),
+      Listview(title: "Food", expenses: [
+        "Banaani 1kg",
+        "Mikropitsa"
+      ], prices: [
+        0.99,
+        0.34
+      ], days: [
+        4,
+        31
+      ]),
+      Listview(title: "Other", expenses: [
+        "Bensaa"
+      ], prices: [
+        50.01
+      ], days: [
+        1
+      ]),
     ]);
   }
 }
 
 class Listview extends StatefulWidget {
   final String title;
-
-  const Listview({Key key, this.title}) : super(key: key);
+  final List<String> expenses;
+  final List<double> prices;
+  final List<int> days;
+  const Listview({Key key, this.title, this.expenses, this.prices, this.days}) : super(key: key);
 
   @override
   _ListStateview createState() => new _ListStateview();
@@ -42,7 +72,10 @@ class _ListStateview extends State<Listview> {
                 Text(
                   widget.title,
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20
+                      ),
                 ),
                 IconButton(
                     icon: Container(
@@ -68,22 +101,84 @@ class _ListStateview extends State<Listview> {
               ],
             ),
           ),
-          new ExpandableContainer(
+          ExpandableContainer(
               expanded: expandFlag,
-              child: ListView.builder(
+              tileAmount: widget.expenses.length,
+              child: 
+                ListView.builder(
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
                     decoration: BoxDecoration(color: Colors.white),
-                    child: ListTile(
-                        title: Text(
-                          "A can of beer $index",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.black),
+                    child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          width: 25,
+                        child:
+                        Text(
+                          widget.days[index].toString(),
+                          style:
+                            TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
+                            ),
+                          ),
                         ),
-                        trailing: Text('10€')),
+                        SizedBox(
+                          width: 45
+                        ),
+                       Container(
+                          height: 50,
+                          width: 60,
+                        child:
+                        Text(
+                          widget.prices[index].toString() + '€',
+                          style:
+                            TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
+                            )
+                        )
+                        ),
+                        SizedBox(
+                          width: 35                    
+                        ),
+                        Container(
+                          height: 50,
+                          width: 135,
+                          child:
+                        Text(
+                          widget.expenses[index],
+                          style:
+                            TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
+                            ),
+                        )
+                        ),
+                        PopupMenuButton(
+                          itemBuilder: (context) => [
+                            PopupMenuItem(
+                              value: 1,
+                              child: Text(
+                                "Muokkaa kulua"
+                              ),
+                            ),
+                          ],
+                          initialValue: 0,
+                          onSelected: (value) { 
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ModifyExpense()),
+                            );
+                            },
+                        )
+                        ]
+                    )
                   );
                 },
-                itemCount: 10,
+                itemCount: widget.expenses.length,
               ))
         ],
       ),
@@ -96,27 +191,77 @@ class ExpandableContainer extends StatelessWidget {
   final double collapsedHeight;
   final double expandedHeight;
   final Widget child;
+  final int tileAmount;
 
   ExpandableContainer({
     @required this.child,
+    @required this.tileAmount,
     this.collapsedHeight = 0.0,
-    this.expandedHeight = 300.0,
-    this.expanded = true,
+    this.expandedHeight = 50.0,
+    this.expanded = false,
   });
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 500),
-      curve: Curves.easeInOut,
-      width: screenWidth,
-      height: expanded ? expandedHeight : collapsedHeight,
-      child: Container(
-        child: child,
-        //   decoration:
-        //       BoxDecoration(border: Border.all(width: 1.0, color: Colors.blue)),
-      ),
+    return Column(
+      children: <Widget>[
+        AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          width: screenWidth,
+          height: expanded ? 50.0 : 0.00,
+          color: Colors.white,
+         child: Container(
+           color: Colors.white,
+            child: Row(
+            //  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+             Text(
+            "Päivä",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              fontSize: 20,
+              backgroundColor: Colors.white
+              ),
+             ), 
+             SizedBox(
+              width: 20
+             ),
+              Text(
+                "Summa",
+                style: TextStyle(
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              fontSize: 20,
+              backgroundColor: Colors.white
+              )
+              ), 
+              SizedBox(
+                width: 25
+              ),
+              Text(
+                "Selite",
+                style: TextStyle(
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              fontSize: 20,
+              backgroundColor: Colors.white
+              )
+              ),
+           ])
+         ),
+        ),
+         AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          color: Colors.black, // Black for now, trying to get rid of extra space on top of list
+          curve: Curves.easeInOut,
+          width: screenWidth,
+          height: expanded ? 50.0 * this.tileAmount.toDouble() : collapsedHeight,
+          child: child
+       )
+      ]
     );
   }
 }
---
+++ lib/views/modify_expense/modify_expense_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:kuluappi/views/add_expense/widgets/SelectCategoryLayout.dart';
+
+class ModifyExpense extends StatelessWidget {
+  const ModifyExpense();
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      SelectCategoryLayout(
+      title: "Muokkaa kulua",
+      body: Center(
+          child: Text(
+        'Add expense',
+        textScaleFactor: 1.7,
+        ),
+      ),
+    );
+  }
+}
