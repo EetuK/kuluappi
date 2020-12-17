8caddf6c0e0479b1f502bbce5fbce04e8adf5bf6
EetuK <eetukoskelainen@hotmail.com>
2020-12-17 15:13:05 +0200

    remove unneeded setting actions

--- lib/views/settings/settings_view.dart
+++ lib/views/settings/settings_view.dart
@@ -20,27 +20,27 @@ class SettingsView extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               RaisedButton(
-                child: Text("Delete all tables and init new"),
+                child: Text("Reset app data"),
                 onPressed: () async {
                   await db.clearDatabaseAndInit();
                   await expenseStore.fetchAvailableYears();
                 },
               ),
-              RaisedButton(
-                child: Text("Add dummy data"),
-                onPressed: () => {db.addDummyData()},
-              ),
-              RaisedButton(
-                child: Text("Test connection"),
-                onPressed: () async {
-                  var data = await getAllExpenses();
-                  Toast.show(
-                      " ${data.map((e) => e.description.toString()).join(",")}",
-                      context,
-                      duration: Toast.LENGTH_LONG,
-                      gravity: Toast.BOTTOM);
-                },
-              ),
+              // RaisedButton(
+              //   child: Text("Add dummy data"),
+              //   onPressed: () => {db.addDummyData()},
+              // ),
+              // RaisedButton(
+              //   child: Text("Test connection"),
+              //   onPressed: () async {
+              //     var data = await getAllExpenses();
+              //     Toast.show(
+              //         " ${data.map((e) => e.description.toString()).join(",")}",
+              //         context,
+              //         duration: Toast.LENGTH_LONG,
+              //         gravity: Toast.BOTTOM);
+              //   },
+              // ),
             ],
           ),
           padding: EdgeInsets.only(left: 10, right: 10),
